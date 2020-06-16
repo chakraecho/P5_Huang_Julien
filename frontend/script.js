3b5546896e7b8150dc5b8d1999d61e2557c0f1f6
Julien Huang <julien.huang@outlook.fr>
2020-06-16 15:33:59 +0200

    feat: added layout for get request on main page -added bootstrap card layout for get request

--- frontend/script.js
+++ frontend/script.js
@@ -13,16 +13,21 @@ req.onreadystatechange = function () {
 
         if (response.length > 0) {//if teddy in stock
             response.forEach((element, index, array)=> {//insert all teddies in a list
-                $('#objectList').append('<li id="object_' + index + '"class="listed-object"></li>');
+              /*  $('#objectList').append('<li id="object_' + index + '"class="listed-object mt-1"></li>');
                 $('#object_' + index).append('<div class="row" id="row_object_' + index + '"></div>');
-                $('#row_object_' + index).append('<div class="col-md-3 col-sm-4"><img class="object_img" src = "' + response[index].imageUrl + '" alt = "image of a teddy" /></div ><div class="col-sm-8 col-md-3"><div class="row"><div class="col-12">' + response[index].name +'</div></div><div class="row mt-auto"><div class="offset-6 col-3">buy </div></div></div>');
-                console.log(element)
+                $('#row_object_' + index).append('<div class="col-3"><img class="object_img" src = "' + response[index].imageUrl + '" alt = "image of a teddy" /></div ><div class="col-8"><div class="row align-items-center h-100"><div class="col-6">' + response[index].name +'</div><div class="offset-2 col-3 offset-lg-2 col-lg-1"><img class="cart" alt="ajouter au panier" src="img/main/add.svg"> </div></div></div>');
+                console.log(element) */
+                $('#objectList').append('<div class="card col-md-5 col-sm-8  ml-2 mr-2 mb-2" id="card_' + index + '"></div>');
+                $('#card_' + index).append('<img class="card-img-top" id="card-img_' + index + '" alt="image de' + response[index].name + '" src="' + response[index].imageUrl + '"/>');
+                $('#card_' + index).append('<div class="card-body" id="card-body_' + index + '"> </div>');
+                $('#card-body_' + index).append('<div class="card-title text-center" id="card-title_' + index + '">' + response[index].name + '</div>');
+                $('#card-body_' + index).append('<div class="row"><button type="button" class="btn btn-success mx-auto" id="add_cart_'+ index +'">Ajouter au panier</button></div>')
+
             })
 
         }
         else if (response.length === 0) {//if no teddy in stock
-            
+            $('#objectList').html("Il n'y a plus d'article disponible!")
         }
     }
 }
-
