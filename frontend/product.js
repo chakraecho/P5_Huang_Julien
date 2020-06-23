9fa49149450a07c3ef2a50405589c430ba451862
Julien Huang <julien.huang@outlook.fr>
2020-06-23 20:47:44 +0200

    feat: changed in cart items js script -separeted js code between index.html and product.html -change: js script for in carts items. there is now 2 array. 1 for items id another for items number

---
+++ frontend/product.js
@@ -0,0 +1,92 @@
+var response;
+const api = "http://localhost:3000/api/teddies"
+
+
+//LOCAL STORAGE
+let cart = localStorage
+
+
+//REQ TO ID
+
+    //PRODUCT PAGE GET /:id
+    let chemin = window.location.search
+    let idProduct = chemin.substring(1)
+    console.log(idProduct)
+    let itemsInCart=[];//create array for items
+    let qtyInCart=[]; //qty in cart
+    let XHR = new XMLHttpRequest
+    XHR.open('GET',api+ '/'+idProduct)
+    XHR.send()
+    XHR.onreadystatechange = function(){
+
+        if(this.readyState == XMLHttpRequest.DONE && this.status == 200){
+            let product = JSON.parse(this.responseText)
+            console.log(this.responseText)
+            console.log(product)
+            $('title').html("oribear "+product.name)
+            $('#product-title').html(product.name)
+            $('#product_body').append('<img class="col-sm-6 col-md-3 offset-md-1" src="'+ product.imageUrl +'" alt="image de '+ product.name + '"/>')
+            $('#product_body').append('<p class="col-md-4 col-sm-8" id="product_description">'+product.description+'</p>')
+            $('#product_body').append('<div class="col-md-3 col-sm-10 d-flex flex-column justify-content-between align-items-center" id="product_cart_col"></div>')
+            $('#product_cart_col').append('<form id="color-select"> </form>')
+            $('#color-select').append('<label for="color-select_menu" id="color-label">Couleur :</label>')
+            $('#color-select').append('<select name="color-select_menu" id="color-select_menu"></select>')
+            $('#color-select_menu').append('<option value="">choisissez une couleur</option>')
+            for(let i=0; i < product.colors.length;i++){
+                $('#color-select_menu').append('<option value="'+product.colors[i]+'">'+product.colors[i]+'</option>')
+            }
+            $('#product_cart_col').append('<h2>'+product.price/100+'€</h2>')
+            $('#product_cart_col').append('<button type="button" class="btn add_cart mx-auto btn-success" id="'+product._id+'" id="'+product._id+'">Ajouter au panier</button>')
+
+        }
+        if (localStorage == null) {//if first time connecting to this website
+            itemsInCart = []
+            qtyInCart = []
+            $('#card_button').html('0')
+        }
+        else if(localStorage.length ==0){
+            itemsInCart = []
+            qtyInCart = []
+            $('#card_button').html('0')
+        }
+        else {
+            itemsInCart = JSON.parse(cart.getItem('inCart'))
+            qtyInCart = JSON.parse(cart.getItem('qtyInCart'))
+            $('#card_button').html(itemsInCart.length)
+    
+        };
+        $('.add_cart').on('click', function (e) {//ADD CART on button listener
+            console.log(cart)
+            if(itemsInCart.includes(this.id)){
+                for(let i=0; i< itemsInCart.length;i++){
+                    if(this.id === itemsInCart[i]){
+                        newQty = qtyInCart[i]
+                        newQty++
+                        qtyInCart[i] = newQty
+                    }
+                }
+            }
+            else{
+                itemsInCart.push(this.id)
+                qtyInCart.push(1)
+            }
+            cart.setItem('inCart', JSON.stringify(itemsInCart))
+            cart.setItem('qtyInCart',JSON.stringify(qtyInCart))
+            let cart_number = 0
+            for(let i=0; i < qtyInCart.length;i++){
+                cart_number += qtyInCart[i]
+            }
+            console.log(JSON.parse(cart.getItem('inCart')))
+            //numbers of items in cart to the nav bar
+            $('#in_cart_count').html(cart_number)
+        });
+    }
+
+
+
+$('#in_cart').on('mouseover',function(){//Fade in Popover incart items
+    $('#in_cart_popover').fadeIn('slow')
+})
+$('#in_cart').on('mouseout',function(){//Fade out Popover incart items
+    $('#in_cart_popover').fadeOut('slow')
+})
--- frontend/script.js
+++ frontend/script.js
@@ -3,24 +3,28 @@ const api = "http://localhost:3000/api/teddies"
 
 //LOCAL STORAGE
 let cart = localStorage
-
+let itemsInCart;//create array for items
+let qtyInCart; //qty in cart
 //request GET to api
 var req = new XMLHttpRequest
 req.open('GET', api)
 req.send()
 req.onreadystatechange = function () {
-    let itemsInCart;//create array for items
+
     if (localStorage == null) {//if first time connecting to this website
         itemsInCart = []
+        qtyInCart = []
         $('#in_cart_count').html('0')
     }
     else if(localStorage.length ==0){
         itemsInCart=[]
+        qtyInCart = []
         $('#in_cart_count').html('0')
     }
     else {
         itemsInCart = JSON.parse(cart.getItem('inCart'))
-        $('#in_cart_count').html(itemsInCart.length)
+        qtyInCart = JSON.parse(cart.getItem('qtyInCart'))
+        $('#in_cart_count').html(itemsInCart.length) ///TO CAHNGE
     };
 
     if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
@@ -60,70 +64,6 @@ req.onreadystatechange = function () {
 };
 
 
-
-//REQ TO ID
-
-var nom = window.location.pathname;
-nom = nom.split("/");
-nom = nom[nom.length - 1];
-nom = nom.substr(0, nom.lastIndexOf("."));
-nom = nom.replace(new RegExp("(%20|_|-)", "g"), "");
-console.log(nom)
-
-if(nom == 'product'){
-    //PRODUCT PAGE GET /:id
-    let chemin = window.location.search
-    let idProduct = chemin.substring(1)
-    console.log(idProduct)
-
-    let XHR = new XMLHttpRequest
-    XHR.open('GET',api+ '/'+idProduct)
-    XHR.send()
-    XHR.onreadystatechange = function(){
-        if(this.readyState == XMLHttpRequest.DONE && this.status == 200){
-            let product = JSON.parse(this.responseText)
-            console.log(this.responseText)
-            console.log(product)
-            $('title').html("oribear "+product.name)
-            $('#product-title').html(product.name)
-            $('#product_body').append('<img class="col-sm-6 col-md-3 offset-md-1" src="'+ product.imageUrl +'" alt="image de '+ product.name + '"/>')
-            $('#product_body').append('<p class="col-md-4 col-sm-8" id="product_description">'+product.description+'</p>')
-            $('#product_body').append('<div class="col-md-3 col-sm-10 d-flex flex-column justify-content-between align-items-center" id="product_cart_col"></div>')
-            $('#product_cart_col').append('<form id="color-select"> </form>')
-            $('#color-select').append('<label for="color-select_menu" id="color-label">Couleur :</label>')
-            $('#color-select').append('<select name="color-select_menu" id="color-select_menu"></select>')
-            $('#color-select_menu').append('<option value="">choisissez une couleur</option>')
-            for(let i=0; i < product.colors.length;i++){
-                $('#color-select_menu').append('<option value="'+product.colors[i]+'">'+product.colors[i]+'</option>')
-            }
-            $('#product_cart_col').append('<h2>'+product.price/100+'€</h2>')
-            $('#product_cart_col').append('<button type="button" class="btn add_cart mx-auto btn-success" id="'+product._id+'" id="'+product._id+'">Ajouter au panier</button>')
-
-        }
-        let itemsInCart;//create array for items
-        if (localStorage == null) {//if first time connecting to this website
-            itemsInCart = []
-            $('#card_button').html('0')
-        }
-        else if(localStorage.length ==0){
-            itemsInCart=[]
-            $('#card_button').html('0')
-        }
-        else {
-            itemsInCart = JSON.parse(cart.getItem('inCart'))
-            $('#card_button').html(itemsInCart.length)
-    
-        };
-        $('.add_cart').on('click', function (e) {//ADD CART on button listener
-            console.log(cart)
-            itemsInCart.push(this.id);
-            cart.setItem('inCart', JSON.stringify(itemsInCart))
-            console.log(JSON.parse(cart.getItem('inCart')))
-            //numbers of items in cart to the nav bar
-            $('#in_cart_count').html(itemsInCart.length)
-        });
-    }
-}
 $('#in_cart').on('mouseover',function(){//Fade in Popover incart items
     $('#in_cart_popover').fadeIn('slow')
 })
